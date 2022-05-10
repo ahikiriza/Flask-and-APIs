fff05d65f46d976952334646cd99d0eb4f2a616a
Ahikiriza Henry <ahikirizahenry5@gmail.com>
2022-05-10 13:16:52 +0300

    added the json file

--- application.py
+++ application.py
@@ -1,23 +1,39 @@
-from flask import Flask, jsonify, redirect, render_template ,request, url_for
+from crypt import methods
+from flask import Flask, jsonify, redirect, render_template ,request, url_for, crypt
 app =Flask(__name__)
 
-@app.route('/')
-def run():
-    return 'hello'
+studentDB = [
+    { "rollno": '11',
+     "name": "Mary Denis",
+     "section": 'A' 
+     },
+    {
+        "rollno": '12',
+        "name": "Phil cousol",
+        "section": 'B'
+    }
+]
+
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'Welcome to the webservice'
+
+@app.route("/student/getstudents", methods=['GET'])
+def getstudents():
+    return jsonify({"stud":studentDB})
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method =="POST":
         user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-        
+        return redirect(url_for("user", usr=user))        
     else:        
         return render_template("login.html")
 
 @app.route('/<usr>')
 def user(usr):
     return f"<h1>{usr}</h1>"
-
+    
 
 if __name__ == '__main__':
     app.run(debug =True)
